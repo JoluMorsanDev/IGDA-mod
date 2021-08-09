563700e3bfad395616a037d747193b6910af03f4
JoluMorsanDev <mo440593@uaeh.edu.mx>
2021-08-09 15:59:33 -0500

    BasicBlocks and recipes

---
+++ src/main/java/net/mcreator/igdamod/item/IGDApowderItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.igdamod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.igdamod.IgdamodModElements;
+
+@IgdamodModElements.ModElement.Tag
+public class IGDApowderItem extends IgdamodModElements.ModElement {
+	@ObjectHolder("igdamod:igd_apowder")
+	public static final Item block = null;
+	public IGDApowderItem(IgdamodModElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("igd_apowder");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
